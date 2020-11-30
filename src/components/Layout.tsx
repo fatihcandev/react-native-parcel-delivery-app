595d02227e2348bbce5d3d464a6079f25c3c566f
fatihcandev <fatihcan.social@gmail.com>
2020-11-30 13:35:07 +0300

    refactor

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { ScrollView } from 'react-native-gesture-handler';
-import { SafeAreaView } from 'react-native-safe-area-context';
 
 import Box from './Box';
 import StyledText from './StyledText';
@@ -12,23 +11,21 @@ interface ILayoutProps {
 
 const Layout: React.FC<ILayoutProps> = ({ headingBig, headingSmall, children }) => {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <Box flex={1} paddingHorizontal="l" paddingVertical="xl" backgroundColor="white">
-        <ScrollView style={{ flex: 1 }} showsVerticalScrollIndicator={false}>
-          {headingBig && (
-            <StyledText variant="h2" marginBottom="m">
-              {headingBig}
-            </StyledText>
-          )}
-          {headingSmall && (
-            <StyledText variant="h3" marginBottom="m">
-              {headingSmall}
-            </StyledText>
-          )}
-          {children}
-        </ScrollView>
-      </Box>
-    </SafeAreaView>
+    <Box flex={1} paddingHorizontal="l" paddingVertical="xl" backgroundColor="white">
+      <ScrollView style={{ flex: 1 }} showsVerticalScrollIndicator={false}>
+        {headingBig && (
+          <StyledText variant="h2" marginBottom="m">
+            {headingBig}
+          </StyledText>
+        )}
+        {headingSmall && (
+          <StyledText variant="h3" marginBottom="m">
+            {headingSmall}
+          </StyledText>
+        )}
+        {children}
+      </ScrollView>
+    </Box>
   );
 };
 
--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -17,7 +17,7 @@ const Loading: React.FC<ILoadingProps> = ({ fullScreen }) => {
       style={{ ...StyleSheet.absoluteFillObject }}
       justifyContent="center"
       alignItems="center"
-      backgroundColor={fullScreen ? 'overlay' : 'transparent'}
+      backgroundColor={fullScreen ? 'overlay' : 'white'}
     >
       <ActivityIndicator color={theme.colors.yellowDark} size="large" />
     </Box>
