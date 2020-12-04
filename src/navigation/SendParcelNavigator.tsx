1b923cb95a4d62b68551d118e943e30351305445
fatihcandev <fatihcan.social@gmail.com>
2020-12-04 16:11:54 +0300

    add horizontal navigation transition

--- src/navigation/SendParcelNavigator.tsx
+++ src/navigation/SendParcelNavigator.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
+import { CardStyleInterpolators, createStackNavigator } from '@react-navigation/stack';
 
 import { SendParcelRoutes } from 'types';
 import { Checkout, DeliveryMethod, ParcelSize } from 'views/SendParcel';
@@ -8,7 +8,10 @@ const SendParcelStack = createStackNavigator<SendParcelRoutes>();
 
 const SendParcelNavigator = () => {
   return (
-    <SendParcelStack.Navigator headerMode="screen">
+    <SendParcelStack.Navigator
+      headerMode="screen"
+      screenOptions={{ cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS }}
+    >
       <SendParcelStack.Screen
         name="ParcelSize"
         component={ParcelSize}
